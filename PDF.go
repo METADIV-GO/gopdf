be4051bba62c75f4bf7df234fdf9af7164be4de6
Nelson Lai <nelsonlai9503@gmail.com>
2024-08-13 23:36:37 +0800

    refactor write table

--- PDF.go
+++ PDF.go
@@ -130,7 +130,6 @@ func (p *PDF) WriteTable(cells []*Cell, padding *Padding) {
 		}
 	}
 
-	x := p.Engine.GetX()
 	y := p.Engine.GetY()
 	maxY := y
 	for _, cell := range cells {
@@ -148,7 +147,7 @@ func (p *PDF) WriteTable(cells []*Cell, padding *Padding) {
 		if p.Engine.GetY() > maxY {
 			maxY = p.Engine.GetY()
 		}
-		p.Engine.SetXY(x+cell.Width, y)
+		p.Engine.SetY(y)
 	}
 	p.Engine.SetY(maxY)
 	if padding != nil && padding.Bottom > 0 {
